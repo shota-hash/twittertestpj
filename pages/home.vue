d4fddbbff7cc33f8c3d88183be38b89f09b1c8fe
鈴木翔太 <suzukishouta@suzukishoutanoMacBook-Pro.local>
2021-09-17 07:55:21 +0900

    commit11

--- pages/home.vue
+++ pages/home.vue
@@ -23,7 +23,7 @@
           <th>ホーム</th>
         </tr>
         <tr v-for="item in messages" :key="item.id">
-          <th>{{item.contact.name}}<img class="logo4" src="~/assets/images/heart.png" @click="toggleBoolean"><span v-if="boolean">0</span><span v-else>1</span><img class="logo4" src="~/assets/images/cross.png" @click="deleteContact(item.id)"><NuxtLink to="/reply"><img class="logo5" src="~/assets/images/detail.png"></NuxtLink><p class="comment_content">{{item.news}}</p></th>
+          <th>{{item.contact.name}}<img class="logo4" src="~/assets/images/heart.png" @click="goodNum(index)"><span>{{good.num}}</span><img class="logo4" src="~/assets/images/cross.png" @click="deleteContact(item.id)"><NuxtLink to="/reply"><img class="logo5" src="~/assets/images/detail.png"></NuxtLink><p class="comment_content">{{item.news}}</p></th>
         </tr>
       </table>
     </div>
@@ -37,9 +37,9 @@ export default {
     return {
       newNews: "",
       messages: [],
-      boolean: true,
       user_id: "",
       contact_id: "",
+      goods :[],
     };
   },
   methods: {
@@ -69,11 +69,12 @@ export default {
       this.getMessage();
     },
     async deleteContact(id) {
-      await this.$axios.delete("http://127.0.0.1:8000/api/message" + id);
+      await this.$axios.delete("http://127.0.0.1:8000/api/message"+ id);
       this.getMessage();
     },
-    toggleBoolean(){
-          this.boolean = !this.boolean;
+    addNum:function(inedex){
+      const good = this.goods[inedex];
+      good.num++;
     },
   },
   created() {
